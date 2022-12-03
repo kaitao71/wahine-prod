17243bb58937027e2a8459d823b6058df50d3cc5
Cheng Kai Tao <chengkaitao@Chengs-MacBook-Pro.local>
2022-12-03 08:54:09 +0800

    added pages

--- backend/views.py
+++ backend/views.py
@@ -28,6 +28,10 @@ def signup(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("bank_account_form")
             return redirect('bank_account_form')
         else:
             print(form.errors)
